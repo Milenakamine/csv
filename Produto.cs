8011a7a9fbf1edf912917c5558114c0ff7b9c5c8
Milenakamine <aakaminemilena@gmail.com>
2020-07-02 18:46:54 -0300

    Finalizado

--- Produto.cs
+++ Produto.cs
@@ -8,14 +8,19 @@ namespace csv {
         //variavel
 
         public int Codigo { get; set; }
+       
         public string Nome { get; set; }
 
         public float Preco { get; set; }
 
+
         private const string PATH = "Database/produto.csv";
+
         //se não existir a pasta, ela é criada a partir desse processo com o !
-        public Produto () {
+        public Produto () 
+        {
             string pasta = PATH.Split ('/') [0];
+
             if (!Directory.Exists (pasta)) {
                 Directory.CreateDirectory (pasta);
             }
@@ -24,19 +29,26 @@ namespace csv {
                 File.Create (PATH).Close ();
             }
         }
+
+
         //cadastrando o produto e arrumado pelo prepararlinha 
-        public void Cadastrar (Produto prod) {
-            var linha = new string[] { PreparLinha (prod) };
+
+        public void Cadastrar (Produto prod) 
+        {
+            var linha = new string[] { PrepararLinha (prod) };
+
             File.AppendAllLines (PATH, linha);
         }
 
-        public List<Produto> Ler () {
+        public List<Produto> Ler()
+         {
             //lista de produtos
             List<Produto> produtos = new List<Produto> ();
 
             string[] linhas = File.ReadAllLines (PATH);
 
-            foreach (string linha in linhas) {
+            foreach (string linha in linhas)
+            {
 
                 //separamos os dados de cada linha com Split
                 string[] dado = linha.Split (";");
@@ -44,50 +56,103 @@ namespace csv {
                 //produtos que vão ser colocados na lista
 
                 Produto p = new Produto ();
-                p.Codigo = Int32.Parse (Separar (dado[0]));
+                p.Codigo = Int32.Parse (Separar (dado[0]) );
                 p.Nome = Separar (dado[1]);
-                p.Preco = float.Parse (Separar (dado[2]));
+                p.Preco = float.Parse (Separar (dado[2]) );
                 //adicionando os produtos 
                 produtos.Add (p);
 
             }
+
             produtos = produtos.OrderBy (y => y.Nome).ToList ();
+
             return produtos;
 
         }
+
+
         //para remover uma linha da lista criada
-        public void Remover (string _termo) {
+        public void Remover (string _termo) 
+        {
             //criação de lista 
-            List<string> linhas = new List<string> ();
+            List<string> linhas = new List<string>();
+            
             using (StreamReader arquivo = new StreamReader (PATH)) {
                 string linha;
-                while ((linha = arquivo.ReadLine ()) != null) {
+                while ((linha = arquivo.ReadLine()) != null) 
+                {
                     linhas.Add (linha);
                 }
             }
-            linhas.RemoveAll (l => l.Contains (_termo));
-            //reescreve o csv de novo 
-            using (StreamWriter output = new StreamWriter (PATH)) {
 
-               output.Write(String.Join(Environment.NewLine, linhas.ToArray()));
-            }
+            linhas.RemoveAll (l => l.Contains(_termo));
+            
+            //reescreve o csv de novo 
+            // using (StreamWriter output = new StreamWriter (PATH))
+                //como acontece a mesma coisa duas vezes é poivel criar um metodo para sua otimização
+                // output.Write (String.Join (Environment.NewLine, linhas.ToArray ()));
+                
+                Reescrever(linhas);
+            
         }
 
-            public List<Produto> Filtrar (string _nome) {
-            return Ler ().FindAll (x => x.Nome == _nome);
+
+        public void Alterar(Produto _produtoAlterado){
+    
+            List<string> linhas = new List<string>();
+
+            using(StreamReader arquivo = new StreamReader(PATH))
+            {
+                string linha;
+                while((linha = arquivo.ReadLine()) != null)
+                {
+                    
+                    linhas.Add(linha);
+                }
             }
 
 
-            private string Separar (string _coluna) {
+            //removendo o produto pelo código
+              linhas.RemoveAll(z => z.Split(";")[0].Contains(_produtoAlterado.Codigo.ToString()));
 
-                //0        1
-                //nome = Iphone
+            // outra possibilidade é:  linhas.RemoveAll(z => z.Split(";")[0].Split("=")[1] == _produtoAlterado.Codigo.ToString());
 
-                return _coluna.Split ("=") [1];
-            }
+            // adiciona o que foi alterado
+            linhas.Add(PrepararLinha(_produtoAlterado) );
 
-            public string PreparLinha (Produto p) {
-                return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
-            }
+            // escreve o csv de novo 
+            Reescrever(linhas);         
+        }
+
+         //metodo de otimização
+          private void Reescrever(List<string> lines){
+
+            using(StreamWriter output = new StreamWriter(PATH))
+            {
+                foreach(string ln in lines)
+                {
+                    output.Write(ln + "\n");
+                }
+            }   
+        }
+
+           public List<Produto> Filtrar(string _nome)
+        {
+            return Ler().FindAll(x => x.Nome == _nome);
+        }
+
+        
+
+        private string Separar (string _coluna) {
+
+            //0        1
+            //nome = Iphone
+
+            return _coluna.Split ("=") [1];
+        }
+
+        public string PrepararLinha (Produto p) {
+            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
         }
     }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace csv
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Produto p2 = new Produto();
-            p2.Codigo= 2;
-            p2.Nome= "Xiaomi";
-            p2.Preco= 4555f;
-            // p2.Cadastrar(p2);
-            p2.Remover("Motorola");
+namespace csv {
+    class Program {
+        static void Main (string[] args) {
+            Produto p2 = new Produto ();
+            p2.Codigo = 2;
+            p2.Nome = "Xiaomi";
+            p2.Preco = 4555f;
 
-            List<Produto> lista = p2.Ler();
+            p2.Cadastrar (p2);
 
-            foreach(Produto item in lista){
+            Produto alterado = new Produto ();
+            alterado.Codigo = 3;
+            alterado.Nome = "Nokia";
+            alterado.Preco = 326f;
 
-                Console.WriteLine($"{item.Nome} - R${item.Preco}.");
+            p2.Alterar (alterado);
+
+            p2.Remover ("Motorola");
+
+
+
+            // List<Produto> lista = p2.Ler ();
+
+            List<Produto> lista = p2.Filtrar("Iphone");
+
+            foreach (Produto item in lista) {
+
+                Console.WriteLine ($"{item.Nome} - R${item.Preco}.");
 
             }
         }
